ea437625b649d244516983038e24a63e6f7afb2a
roopsie <roopkirandhillon1@gmail.com>
2025-11-02 16:30:36 -0500

    Add JerseyConfig class to configure JAX-RS for API endpoints

---
+++ src/main/java/com/ecommerce/JerseyConfig.java
@@ -0,0 +1,13 @@
+package com.ecommerce;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import jakarta.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api")
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig() {
+        packages("com.ecommerce");
+    }
+    
+}
