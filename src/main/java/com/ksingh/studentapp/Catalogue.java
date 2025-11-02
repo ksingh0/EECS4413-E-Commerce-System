780edd823136c05dbeb128ae52bf31812ae26494
bonilla-kimberly <kim29@my.yorku.ca>
2025-11-02 14:58:26 -0500

    Implemented CatalogueDAO, CatalogueController, Catalogue.java and in context.xml replaced student.db with catalogue.db

---
+++ src/main/java/com/ksingh/studentapp/Catalogue.java
@@ -0,0 +1,107 @@
+package com.ksingh.studentapp;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Catalogue {
+	//model class for items in Catalogue
+	@JsonProperty("item_id")
+	private int item_id; 
+	@JsonProperty("item_name")
+	private String item_name;
+	@JsonProperty("current_bid")
+	private double current_bid;
+	@JsonProperty("auction_type")
+	private String auction_type;
+	@JsonProperty("remaining_time")
+	private String remaining_time;
+	@JsonProperty("item_description")
+	private String item_description;
+	@JsonProperty("shipping_time")
+	private String shipping_time;
+	@JsonProperty("end_date")
+	private String end_date;
+	@JsonProperty("initial_price")
+	private double initial_price;
+	
+	//getters and setters
+	public int getItemID() {
+		return item_id;
+	}
+	
+	public void setItemID(int item_id) {
+		this.item_id = item_id;
+	}
+	
+	public String getItemName() {
+		return item_name;
+	}
+	
+	public void setItemName(String item_name) {
+		this.item_name = item_name;
+	}
+	
+	public double getCurrentBid() {
+		return current_bid;
+	}
+	
+	public void setCurrentBid(double current_bid) {
+		this.current_bid = current_bid;
+	}
+	
+	public String getAuctionType() {
+		return auction_type;
+	}
+	
+	public void setAuctionType(String auction_type) {
+		this.auction_type = auction_type;
+	}
+	
+	public String getRemainingTime() {
+		return remaining_time;
+	}
+	
+	public void setRemainingTime(String remaining_time) {
+		this.remaining_time = remaining_time;
+	}
+	
+	public String getItemDescription() {
+		return item_description;
+	}
+	
+	public void setItemDescription(String item_description) {
+		this.item_description = item_description;
+	}
+	
+	public String getShippingTime() {
+		return shipping_time;
+	}
+	
+	public void setShippingTime(String shipping_time) {
+		this.shipping_time = shipping_time;
+	}
+	
+	public String getEndDate() {
+		return end_date;
+	}
+	
+	public void setEndDate(String end_date) {
+		this.end_date = end_date;
+	}
+	
+	public double getInitialPrice() {
+		return initial_price;
+	}
+	
+	public void setInitialPrice(double initial_price) {
+		this.initial_price = initial_price;
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/ksingh/studentapp/CatalogueDAO.java
@@ -0,0 +1,124 @@
+package com.ksingh.studentapp;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class CatalogueDAO {
+	
+	public List<Catalogue> readAllItems() {
+		//Columns in items table from Catalogue Database
+		String sql = "SELECT item_id, item_name, current_bid, auction_type, remaining_time, item_description, shipping_time, end_date, initial_price FROM items";
+		List<Catalogue> items = new ArrayList<>();
+
+		try (Connection conn = DatabaseConnection.connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)){
+
+			while (rs.next()) {
+				Catalogue item = new Catalogue();
+				item.setItemID(rs.getInt("item_id"));
+				item.setItemName(rs.getString("item_name"));
+				item.setCurrentBid(rs.getDouble("current_bid"));
+				item.setAuctionType(rs.getString("auction_type"));
+				item.setRemainingTime(rs.getString("remaining_time"));
+				item.setItemDescription(rs.getString("item_description"));
+				item.setShippingTime(rs.getString("shipping_time"));
+				item.setEndDate(rs.getString("end_date"));
+				item.setInitialPrice(rs.getDouble("initial_price"));
+				items.add(item);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return items;
+	}
+	public Catalogue createItem(Catalogue item) {
+		//Insert new item into database
+		String sql = "INSERT INTO items (item_name, current_bid, auction_type, remaining_time, item_description, shipping_time, end_date, initial_price) VALUES(?,?,?,?,?,?,?,?)";
+
+		try (Connection conn = DatabaseConnection.connect();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, item.getItemName());
+			pstmt.setDouble(2, item.getCurrentBid());
+			pstmt.setString(3, item.getAuctionType());
+			pstmt.setString(4, item.getRemainingTime());
+			pstmt.setString(5, item.getItemDescription());
+			pstmt.setString(6, item.getShippingTime());
+			pstmt.setString(7, item.getEndDate());
+			pstmt.setDouble(8, item.getInitialPrice());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return item;
+	}
+	
+	public Catalogue readItem(int id) {
+		// Read specific item by ID from Catalogue
+		String sql = "SELECT item_name, current_bid, auction_type, remaining_time, item_description, shipping_time, end_date, initial_price FROM items WHERE item_id = ?";
+		Catalogue item = null;
+		
+		try (Connection conn = DatabaseConnection.connect();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// Set the corresponding parameter
+			pstmt.setInt(1, id);
+			try (ResultSet rs = pstmt.executeQuery()) {
+				// Check if a result was returned
+				if (rs.next()) {
+					item = new Catalogue();
+					item.setItemID(id);
+					item.setItemName(rs.getString("item_name"));
+					item.setCurrentBid(rs.getDouble("current_bid"));
+					item.setAuctionType(rs.getString("auction_type"));
+					item.setRemainingTime(rs.getString("remaining_time"));
+					item.setItemDescription(rs.getString("item_description"));
+					item.setShippingTime(rs.getString("shipping_time"));
+					item.setEndDate(rs.getString("end_date"));
+					item.setInitialPrice(rs.getDouble("initial_price"));
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return item;
+	}
+	public Catalogue updateItem(int id, Catalogue item) {
+		//update specific item by ID from Catalogue
+		String sql = "UPDATE items SET item_name = ?, current_bid = ?, auction_type = ?, remaining_time = ?, item_description = ?, shipping_time = ?, end_date = ?, initial_price = ? WHERE item_id =?";
+
+						try (Connection conn = DatabaseConnection.connect();
+							PreparedStatement pstmt = conn.prepareStatement(sql)) {
+							pstmt.setString(1, item.getItemName());
+							pstmt.setDouble(2, item.getCurrentBid());
+							pstmt.setString(3, item.getAuctionType());
+							pstmt.setString(4, item.getRemainingTime());
+							pstmt.setString(5, item.getItemDescription());
+							pstmt.setString(6, item.getShippingTime());
+							pstmt.setString(7, item.getEndDate());
+							pstmt.setDouble(8, item.getInitialPrice());
+							pstmt.setInt(9, id);
+							//update the item record
+							pstmt.executeUpdate();
+						} catch (SQLException e) {
+							System.out.println(e.getMessage());
+						}
+						return item;
+	}
+	public void deleteItem(int id) {
+		//Delete a specific item by ID from Catalogue
+		String sql = "DELETE FROM items WHERE item_id = ?";
+
+		try (Connection conn = DatabaseConnection.connect();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// Set the corresponding parameter
+			pstmt.setInt(1, id);
+			// Delete the item record
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
