be9552f513ae146b4610d1efbdfb1c66ad5b7c48
roopsie <roopkirandhillon1@gmail.com>
2025-11-02 12:32:19 -0500

    Implement UserController

--- src/main/java/com/ecommerce/UserController.java
+++ src/main/java/com/ecommerce/UserController.java
@@ -1,5 +1,44 @@
 package com.ecommerce;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
 
+@Path("/users")
 public class UserController {
+	private UserDAO userDAO = new UserDAO();
 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getAllUsers() {
+		return userDAO.findAll();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User createUser(User user) {
+		return userDAO.create(user);
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUser(@PathParam("id") long id) {
+		return userDAO.findById(id);
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User updateUser(@PathParam("id") long id, User user) {
+		return userDAO.update(id, user);
+	}
+
+	@DELETE
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteUser(@PathParam("id") long id) {
+		userDAO.delete(id);
+	}
 }
